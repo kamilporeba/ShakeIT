24d169e51353f9cd3214eab2acc10a0089262107
Kamil Poręba <kamil.poreba@icloud.com>
2016-09-24 18:26:05 +0200

    Initial Commit

---
+++ ShakeIT/ShakeIT.h
@@ -0,0 +1,19 @@
+//
+//  ShakeIT.h
+//  ShakeIT
+//
+//  Created by Kamil Poręba on 24.09.2016.
+//  Copyright © 2016 Kamil Poręba & Rafał Prążyński. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ShakeIT.
+FOUNDATION_EXPORT double ShakeITVersionNumber;
+
+//! Project version string for ShakeIT.
+FOUNDATION_EXPORT const unsigned char ShakeITVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShakeIT/PublicHeader.h>
+
+
